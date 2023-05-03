234b76ace7f0d2a8db43e0da359d44bd43028185
Teruchiki <t>
2023-05-03 14:46:55 +0900

    Update types

--- types/formToObject.d.ts
+++ types/formToObject.d.ts
@@ -1,5 +1,6 @@
 type ConflictResolver = (targetPonter: string, original: {}, current: any, next: any, localPointer: string) => any;
 export declare function formToObject<T extends {} = {}>(sourceElement: Element, options?: {
+    attribute?: string;
     conflictResolver?: ConflictResolver;
 }): T;
 export {};
